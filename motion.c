563e57ac71286328b903053a48b5ab2562374bfd
Emil Lal <38190245+mrdunker@users.noreply.github.com>
2023-10-07 17:01:00 +0530

    Add files via upload

--- motion.c
+++ motion.c
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+
 void delaytime(int);
 void readpindetails();
 
@@ -12,15 +12,23 @@ int main()
 void readpindetails() {
 
     int led_pin,sensor_pin,led_pin_reg;
-    led_pin = 0;
+    led_pin = 0; // initialize the output pin as LOW initially
     led_pin_reg = led_pin*2;
     //printf("Motion sensor-based room light control started.\n");
-    asm(
+
+    asm volatile(
 	"or x30, x30, %0\n\t" 
-	:"=r"(led_pin_reg));
-    asm(
+	:
+	:"r"(led_pin_reg)
+	:"x30"
+	);
+
+    asm volatile(
 	"andi %0, x30, 1\n\t"
-	:"=r"(sensor_pin));
+	:"=r"(sensor_pin)
+	:
+	:
+	);
 	
     while (1) {
 
@@ -30,9 +38,12 @@ void readpindetails() {
             //printf("Motion detected. Light turned ON.\n");
             led_pin = 1;
             led_pin_reg = led_pin*2;
-            asm(
+            asm volatile(
 		"or x30, x30, %0\n\t" 
-		:"=r"(led_pin_reg));
+		:
+		:"r"(led_pin_reg)
+		:"x30"
+		);
             delaytime(3000);// Almost 6 second delay is given
             // You can add a delay here to control how long the light stays on
         } else {
@@ -41,9 +52,12 @@ void readpindetails() {
             //printf("No motion detected. Light turned OFF.\n");
             led_pin = 0;
             led_pin_reg = led_pin*2;
-            asm(
+            asm volatile(
 		"or x30, x30, %0\n\t" 
-		:"=r"(led_pin_reg));
+		:
+		:"r"(led_pin_reg)
+		:"x30"
+		);
         }
     }
 
