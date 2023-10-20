ef0abed72005914171043c5b8c744a9f1e84a4f9
Emil Lal <38190245+mrdunker@users.noreply.github.com>
2023-10-20 19:46:31 +0530

    Add files via upload

--- motion.c
+++ motion.c
@@ -1,12 +1,8 @@
-
-int led_pin,sensor_pin,led_pin_reg;
-
-void delaytime(int);
-void readpindetails();
-
 int main()
-{   
- 
+{
+
+ int led_pin,sensor_pin,led_pin_reg,i,j,reset_high;
+
  led_pin = 0; // initialize the output pin as LOW initially
  led_pin_reg = led_pin*2;
  
@@ -18,58 +14,57 @@ int main()
 	);
 
  while(1){
-    readpindetails();
-    }
-    return 0;
-        
-}
-void readpindetails() {
-
-
-    //"Motion sensor-based room light control started"
-
-    asm volatile(
+    //Off switch- if the reset_high is low it should be off fully.
+        asm volatile(
 	"andi %0, x30, 1\n\t"
+	:"=r"(reset_high)
+	:
+	:
+	);
+    //"Motion sensor-based room light control started"
+    asm volatile(
+	"andi %0, x30, 2\n\t"
 	:"=r"(sensor_pin)
 	:
 	:
 	);
 
-        if (sensor_pin == 1) {
+        if ((sensor_pin == 1) && (reset_high ==1)) {
             // Motion detected, turn on the room light
             //digitalWrite(LIGHT_PIN, HIGH);
             //printf("Motion detected. Light turned ON.\n");
             led_pin = 1;
-            led_pin_reg = led_pin*2;
+            led_pin_reg = led_pin*4;
             asm volatile(
 		"or x30, x30, %0\n\t" 
 		:
 		:"r"(led_pin_reg)
 		:"x30"
 		);
-            delaytime(3000);// Almost 6 second delay is given
-            // You can add a delay here to control how long the light stays on
-        } else {
-            // No motion detected, turn off the room light
+		
+	// Almost 6 second delay is given
+        // You can add a delay here to control how long the light stays on
+	    for (i = 0; i < 3000; i++) {
+        	for (j = 0; j < 1000000; j++) {
+            	// Adding a loop inside to approximate seconds
+        	}
+    	    }
+    	}//end of if statement
+    	else
+    	{
+    	    //No motion detected or manual switch is off, turn off the room light
             //digitalWrite(LIGHT_PIN, LOW);
             //printf("No motion detected. Light turned OFF.\n");
             led_pin = 0;
-            led_pin_reg = led_pin*2;
+            led_pin_reg = led_pin*4;
             asm volatile(
 		"or x30, x30, %0\n\t" 
 		:
 		:"r"(led_pin_reg)
 		:"x30"
-		);
-        }
-
+		);	
+    	}//end of else statement
+    	}//end while loop
+    	
+    return 0;
 }
-void delaytime(int seconds) {
-    int i, j;
-    for (i = 0; i < seconds; i++) {
-        for (j = 0; j < 1000000; j++) {
-            // Adding a loop inside to approximate seconds
-        }
-    }
-}
-
